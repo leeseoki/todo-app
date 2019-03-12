887c02f68fd826e099ee26ff63cb0534bd1ec457
Pham Thi Van <phamthivan@Vans-Mac.local>
2019-03-12 17:32:20 +0700

    Updates

---
+++ precache-manifest.9ba47d034b256383915303f257192a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "b4c3e3d9dba688e6270d",
+    "url": "/todo-app/static/js/main.2229a726.chunk.js"
+  },
+  {
+    "revision": "00051d2d5ea916edb14d",
+    "url": "/todo-app/static/js/2.783389a1.chunk.js"
+  },
+  {
+    "revision": "b4c3e3d9dba688e6270d",
+    "url": "/todo-app/static/css/main.afb24292.chunk.css"
+  },
+  {
+    "revision": "e80e4ea6f1489c703b8da2d7d639c18c",
+    "url": "/todo-app/index.html"
+  }
+];
